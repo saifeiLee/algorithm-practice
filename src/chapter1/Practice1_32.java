62622fbc1c70d1a8251c50c64690bd3cbfc41787
waltermitty <waltermitty121906@gmail.com>
2022-01-09 22:14:16 +0800

    add: 1.32

--- src/chapter1/Practice1_32.java
+++ src/chapter1/Practice1_32.java
@@ -1,5 +1,6 @@
 package chapter1;
 
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdStats;
 
 public class Practice1_32 {
@@ -12,7 +13,35 @@ public class Practice1_32 {
         }
     }
 
-    public static void main(String[] args) {
+    public static void histogram(double[] values, int n, double l, double r) {
+        int[] counts = new int[n];
+        for (int i = 0; i < values.length; i++) {
+            int k = getInterval(values[i], n, l, r);
+            if (k >= 0) {
+                counts[k]++;
+            }
+        }
+        int maxCount = StdStats.max(counts);
 
+        StdDraw.setCanvasSize(1024, 1024);
+        StdDraw.setXscale(l, r);
+        StdDraw.setYscale(0, maxCount);
+        // 每个柱状图的宽度
+        double w = (r - l) / n;
+        for (int i = 0; i < n; i++) {
+            double x = l + (i + 0.5) * w,
+                    y = counts[i] / 2.0,
+                    rw = 0.5 * w,
+                    rh = counts[i] / 2.0;
+
+            StdDraw.filledRectangle(x, y, rw, rh);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        double  l = 1.0, r = 10.0;
+        double[] values = {1.0, 1.0, 2.0, 3.0, 4.0};
+        histogram(values, n, l, r);
     }
 }
