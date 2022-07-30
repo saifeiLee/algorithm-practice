f28978a83df19f0edf9763a359e746ed9f38d4aa
lisaifei <waltermitty121906@gmail.com>
2022-07-31 00:03:03 +0800

    add: insertion sort

---
+++ src/chapter2/section1/InsertionSort.java
@@ -0,0 +1,13 @@
+package chapter2.section1;
+
+@SuppressWarnings("unchecked")
+public class InsertionSort {
+    public static void sort(Comparable[] a)
+    {
+        int N = a.length;
+        for (int i = 1; i < N; i++) {
+            for (int j = i; j < 0 && less(a[j], a[j - 1];j--))
+                exch(a, j, j - 1);
+        }
+    }
+}
